6f474bfa450a71e840bcdce0694a709e3483f9e9
PPz <ppzreboot@qq.com>
2025-02-18 18:30:20 +0800

    feat: k_colors_oob

--- lib/mod.ts
+++ lib/mod.ts
@@ -3,4 +3,7 @@ export * from './input'
 export * from './k-colors'
 export * from './output'
 
+import k_colors_oob from './k-colors'
+export default k_colors_oob
+
 export { type I_cluster, type I_range, calc_range } from 'k-means-pp'
